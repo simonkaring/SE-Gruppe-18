d2fa5f80df7b876165c8b712b49c795e3176f6b5
Rasmus Meldgaard <MrKetuz@users.noreply.github.com>
2021-04-12 16:05:41 +0200

    Tilføjet test til GUI og markering af liste

--- src/main/java/view/Controller.java
+++ src/main/java/view/Controller.java
@@ -1,4 +1,63 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
+import java.io.IOException;
+import java.util.Observable;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
+
+
 public class Controller {
+
+
+    //Initialize objects in GUI
+    @FXML private ListView listView;
+    @FXML private TextArea textArea;
+    @FXML private Button jButton;
+
+    public Controller() throws IOException {
+    }
+
+    @FXML private void initialize()
+    {
+        listView.getItems().addAll("Test","Test2","Test3","Test4","Test5");
+    }
+
+    public void listViewButtonPushed(){
+        String textAreaString = "";
+
+        ObservableList listOfItems = this.listView.getSelectionModel().getSelectedItems();
+
+        for (Object item : listOfItems)
+        {
+            textAreaString += String.format("%s%n",(String) item);
+        }
+
+        textArea.setText(textAreaString);
+    }
+
+
+
+    //Code for adding stuff to listView
+
+
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Sample Text");
+        primaryStage.setScene(new Scene(root, 720, 480));
         primaryStage.show();
     }
 
