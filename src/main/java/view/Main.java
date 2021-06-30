85fc6fc950ea6344457be677fc964bea58d9da25
Simon <simonkaring@gmail.com>
2021-06-30 21:10:35 +0200

    Deleted newMain

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -21,4 +21,8 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+//    public static void main(String[] args) {
+//        Main.main(args);
+//    }
+
 }
--- src/main/java/view/newMain.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-public class newMain {
-    public static void main(String[] args) {
-        Main.main(args);
-    }
-}
